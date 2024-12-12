20674c6e9802d515db3dd47fcc0cf19602b5bc9b
Weng Rui Hao <usually12336@gmail.com>
2024-12-12 12:31:59 +0800

    更新websocketServer2

--- Convlo-Backend/src/main/java/chat/websocket/WebSocketServer2.java
+++ Convlo-Backend/src/main/java/chat/websocket/WebSocketServer2.java
@@ -28,40 +28,34 @@ public class WebSocketServer2 {
 
 	@Autowired
 	private SendMessageMQSender rabbitMqSender;
-	
+
 	@Autowired
 	private ChatService chatService;
 
 	// 儲存聊天室ID與對應的所有sessionId
-	private ConcurrentHashMap<String, Set<String>> chatRoom=new ConcurrentHashMap<>();
+	private ConcurrentHashMap<String, Set<String>> chatRoom = new ConcurrentHashMap<>();
 	// 儲存每個用戶ID與對應的WebSocket session
-	private ConcurrentHashMap<String, Session> userSession=new ConcurrentHashMap<>();
-	
-	private Session session;
+	private ConcurrentHashMap<String, Session> userSession = new ConcurrentHashMap<>();
+
 	private String username;
 
 	@OnOpen
 	public void onOpen(Session session) throws IOException {
 		String token = session.getRequestParameterMap().get("token").get(0);
 		this.username = JwtUtil.getUsernameFromToken(token);
-		this.session=session;
-		
-		
-		
+
 		// 記錄該用戶的 WebSocket 連接
 		userSession.put(username, session);
 
 		// 查詢用戶擁有的所有聊天室
-		Set<String> chatRooms= chatService.findAllChatByUser(username).stream()
-										.map(chatDto->chatDto.getChatId())
-										.map(ChatId->ChatId.toString())
-										.collect(Collectors.toSet());
+		Set<String> chatRooms = chatService.findAllChatByUser(username).stream().map(ChatId -> ChatId.toString())
+				.collect(Collectors.toSet());
 		System.out.println(chatRooms);
-		for (String roomId : chatRooms) {		
+		for (String roomId : chatRooms) {
 			chatRoom.computeIfAbsent(roomId, k -> ConcurrentHashMap.newKeySet()).add(username);
-	        System.out.println("用戶 " + username + " 加入聊天室: " + roomId);
-	        System.out.println("目前聊天室 " + roomId + " 中有 " + chatRoom.get(roomId).size() + " 人");
-	    }
+			System.out.println("用戶 " + username + " 加入聊天室: " + roomId);
+			System.out.println("目前聊天室 " + roomId + " 中有 " + chatRoom.get(roomId).size() + " 人");
+		}
 	}
 
 	@OnMessage
@@ -72,28 +66,20 @@ public class WebSocketServer2 {
 
 	@OnClose
 	public void onClose() {
-		// 當連接關閉時，從 roomSessions 中移除該 session
-//		for (String roomId : chatRooms) {
-//			chatRoom.get(roomId).remove(username);
-//			System.out.println("目前聊天室 " + roomId + " 中剩餘 " + chatRoom.get(roomId).size() + " 人");
-//		}
-//		userSession.remove(username);
+		System.out.println(username+"已斷開連線");
 	}
 
 	@OnError
-	public void onError(Session session, Throwable throwable) {
-		// 處理錯誤
-//		for (String roomId : chatRooms) {
-//			chatRoom.get(roomId).remove(username);
-//		}
+	public void onError() {
+		System.err.println(username+"已斷開連線");
 	}
 
-	public void SendMessage(String roomId,String message) {
-		Set<String> members=chatRoom.get(roomId);
-		if(members!=null) {
-			members.forEach(username->{
-				Session session=userSession.get(username);// 查找 session
-				if(session!=null&&session.isOpen()) {
+	public void SendMessage(String roomId, String message) {
+		Set<String> members = chatRoom.get(roomId);
+		if (members != null) {
+			members.forEach(username -> {
+				Session session = userSession.get(username);// 查找 session
+				if (session != null && session.isOpen()) {
 					session.getAsyncRemote().sendText(message);
 				}
 			});
